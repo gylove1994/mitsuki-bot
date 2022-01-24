7d3203f0bcac80323ae029164be61c1a946d920c
gylove1994 <gylove1994@acgsteps.com>
2022-01-25 04:23:51 +0800

    增加了新的中间件：permissions

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { Mitsuki } from './types/mitsuki'
+import { permissions,Permission } from './middleware/permissions'
 import path from 'path'
-async function app() {
-    const mitsuki = await Mitsuki.setup(path.join(__dirname, "./config/mitsukiSetting.json"))
 
-    mitsuki.addController("FriendMessage").addMiddleware((msg) => { return msg }).setMainProcess((msg) => { console.log("hello_world") })
-    
+async function app() {
+    const mitsuki = await Mitsuki.setup(path.join(__dirname,"./config/mitsukiSetting.json"))
+    mitsuki.addController("FriendMessage").addMiddleware(permissions).setMainProcess((context)=>{
+        mitsuki.logger.info(context.getMiddlewareOutput<Permission>("permissions").permissions)
+    })
     mitsuki.ready()
 }
 
---
+++ src/middleware/permissions.ts
@@ -0,0 +1,31 @@
+import fs from 'node:fs';
+import { Data, MsgType, MiddlewareOutput } from './../types/controller';
+import path from 'path'
+interface permissionList {
+    admin: number[],
+    ec: number[],
+    block: number[]
+}
+export type Permission = {permissions:string}
+export function permissions<T extends MsgType>(msg: Data<T>): MiddlewareOutput {
+    let list: permissionList;
+    if (fs.existsSync(path.join(__dirname, "../config/permissions.json")))
+        list = JSON.parse(fs.readFileSync(path.join(__dirname, "../config/permissions.json"), "utf-8"))
+    else throw new Error("permissionList配置文件不存在")
+    if (msg.type == "FriendMessage"){
+        for(let i = 0 ; i<list.admin.length ; i++){
+            if(list.admin[i] == msg.sender.id)
+            return {middlewareName:"permissions",output:{permissions:"admin"}} as MiddlewareOutput
+        }
+        for(let i = 0 ; i<list.ec.length ; i++){
+            if(list.admin[i] == msg.sender.id)
+            return {middlewareName:"permissions",output:{permissions:"ec"}} as MiddlewareOutput
+        }
+        for(let i = 0 ; i<list.block.length ; i++){
+            if(list.admin[i] == msg.sender.id)
+            return {middlewareName:"permissions",output:{permissions:"block"}} as MiddlewareOutput
+        }
+        return {middlewareName:"permissions",output:{permissions:"user"}} as MiddlewareOutput
+    }
+    return {middlewareName:"permissions",output:{permissions:"notHandle"}} as MiddlewareOutput
+}
