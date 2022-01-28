fb576727e547ad72b5b41efa73782a8b1a8a16ff
gylove1994 <gylove1994@acgsteps.com>
2022-01-28 15:11:05 +0800

    bug修复：修复权限判断错误的问题

--- src/types/command.ts
+++ src/types/command.ts
@@ -1,11 +1,18 @@
+import { Mitsuki } from './mitsuki';
+import { MsgType, Context } from './controller';
 import { Permission } from './../middleware/permissions';
-class Commands {
-    private commands: SingleCommand[];
-    private defaultCommand?:SingleCommand;
-    constructor() {
-        this.commands = [];
+export type Action<T extends MsgType> = (() => void) | ((context:Context<T>,mitsuki:Mitsuki) => void)
+class Commands<T extends MsgType> {
+    private commands: SingleCommand<T>[]
+    private defaultCommand?:SingleCommand<T>
+    private context:Context<T>
+    private mitsuki:Mitsuki
+    constructor(context:Context<T>,mitsuki:Mitsuki) {
+        this.commands = []
+        this.context = context
+        this.mitsuki = mitsuki
     }
-    public addCommand(command?: string, singleCommand?: SingleCommand): SingleCommand {
+    public addCommand(command?: string, singleCommand?: SingleCommand<T>): SingleCommand<T> {
         if (command == undefined && singleCommand == undefined)
             throw new Error("参数不能为空！");
         if (command != undefined && singleCommand != undefined)
@@ -15,17 +22,17 @@ class Commands {
                 if(this.commands[i].getCommand() == command)
                     throw new Error("命令组内存在重复命令！");
             }
-            const _singleCommand = new SingleCommand(command);
+            const _singleCommand = new SingleCommand<T>(command);
             this.commands.push(_singleCommand);
             return _singleCommand;
         }
         else {
-            this.commands.push(singleCommand as SingleCommand);
-            return singleCommand as SingleCommand;
+            this.commands.push(singleCommand as SingleCommand<T>);
+            return singleCommand as SingleCommand<T>;
         }
     };
     public getSingleCommand() {
-        const singleCommand = new SingleCommand();
+        const singleCommand = new SingleCommand<T>();
         this.commands.push(singleCommand);
         return singleCommand;
     };
@@ -33,52 +40,56 @@ class Commands {
         this.defaultCommand = new SingleCommand("default")
         return this.defaultCommand
     }
-    public async doCommand(com?: string,permissionLv?:number, options?: string[]) {
+    public async doCommand(com?: string,permissionLv?:number) {
         if(com === undefined){
             if(this.defaultCommand !== undefined){
-                this.defaultCommand.doAction()
+                this.defaultCommand.doAction(this.context,this.mitsuki)
                 return
             }
             return
         }
         for (let i = 0; i < this.commands.length; i++) {
-            if (await this.commands[i].checkCommand(com,permissionLv, options)) return
+            if (await this.commands[i].checkCommand(com,this.context,this.mitsuki,permissionLv)) return
         }
-        if(this.defaultCommand !== undefined) this.defaultCommand.doAction()
+        if(this.defaultCommand !== undefined) this.defaultCommand.doAction(this.context,this.mitsuki)
     }   
 }
 
-class SingleCommand {
+class SingleCommand<T extends MsgType> {
     private command?: string;
-    private action?: (options?: string[]) => void;
+    private action?: Action<T>;
     private permissionLv?:number
     constructor(command?: string) {
         if (command != undefined)
             this.command = command;
     };
-    public addAction(action: (options?: string[]) => void) {
+    public addAction(action: Action<T>) {
         this.action = action;
     }
-    public async doAction(options?: string[]) {
+    public async doAction(context:Context<T>,mitsuki:Mitsuki) {
         if (this.action != undefined)
-            await this.action(options)
+            await this.action(context,mitsuki)
         else
             throw new Error("未定义命令所调用的函数！");
     };
-    public async checkCommand(command: string,permissionLv?:number,options?: string[]) {
+    public async checkCommand(command: string,context:Context<T>,mitsuki:Mitsuki,permissionLv?:number) {
         if (command == this.command ) {
             if(permissionLv !== undefined && this.permissionLv !== undefined){
-                if(this.permissionLv >= permissionLv){
-                    await this.doAction(options);
+                if(permissionLv >= this.permissionLv){
+                    await this.doAction(context,mitsuki);
                     return true;
-                }else return false;
+                }else{
+                    context.msg.reply!("【权限不足】您目前的权限级别为：" + permissionLv +"，执行所需最低权限级别为："+this.permissionLv)
+                    return false;
+                }
             }else if(this.permissionLv !== undefined && permissionLv == undefined){
                 throw new Error("对于设置了权限的命令"+this.getCommand+"却未传入权限参数")
             }
             else if(permissionLv !== undefined && permissionLv < 0){
+                context.msg.reply!("【权限不足】您目前处于黑名单中！")
                 return false;
             }else if(this.permissionLv == undefined){
-                await this.doAction(options);
+                await this.doAction(context,mitsuki);
                 return true;
             }
         } else {
